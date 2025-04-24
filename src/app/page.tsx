b2bfee4d4b792635c4def06bf1b805088c88dc85
nhakhoagiadinh <nhakhoagiadinh2025@gmail.com>
2025-04-24 18:42:58 +0700

    Fix lỗi cú pháp page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-  <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-white">
-    <h1 className="text-4xl font-bold text-blue-700 mb-4 text-center">
-      Nha Khoa Gia Đình
-    </h1>
-    <p className="text-lg text-gray-700 text-center">
-      Chăm sóc nụ cười của bạn với dịch vụ uy tín – tận tâm – chuyên nghiệp
-    </p>
-  </main>
-)
-     
+    <main>
+      <p>Chăm sóc nụ cười của bạn với dịch vụ uy tín – tận tâm – chuyên nghiệp</p>
+    </main>
+  );
+}
+
